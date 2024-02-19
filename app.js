550b9ffd30f6c5adb96aa5e36e358c7b7fe494ef
Anastasiia <na.konsta@gmail.com>
2024-02-19 20:44:55 +0100

    fix: try to fix cors error

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ const imageRoute = require('./routes/images');
 
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'device-remember-token',
+    'Access-Control-Allow-Origin',
+    'Origin',
+    'Accept',
+  ],
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/recipes', recipeRoute);
